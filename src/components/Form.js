1e03d9a0bcde9a1bbdf67645dd7d978bab5adff5
Camilo U <cauribeva@unal.edu.co>
2021-09-13 15:05:21 -0500

    Agregada tabla que se actualiza con cada nuevo registro

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,8 +3,21 @@ import { useForm } from "react-hook-form";
 
 export function Form() {
   const { register, handleSubmit } = useForm();
-  const [result, setResult] = useState("");
-  const onSubmit = (data) => setResult(JSON.stringify(data));
+  const [result, setResult] = useState([]);
+  const onSubmit = (data) => {
+    if (data.Edad > 50) {
+      data.Categoria = "Mayor";
+    } else if (data.Edad <= 50 && data.Edad >= 31) {
+      data.Categoria = "Adulto";
+    } else if (data.Edad > 13 && data.Edad <= 30) {
+      data.Categoria = "Joven";
+    } else if (data.Edad >= 0 && data.Edad <= 13) {
+      data.Categoria = "Niño";
+    } else if (data.Edad < 0) {
+      data = {};
+    }
+    setResult([...result, data]);
+  };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -15,14 +28,32 @@ export function Form() {
       <input {...register("Edad")} placeholder="Edad" />
       <br></br>
       <label>Ocupación: </label>
-      <select {...register("category")}>
+      <select {...register("Ocupacion")}>
         <option value="Estudiante">Estudiante</option>
         <option value="Empleado">Empleado</option>
         <option value="Jubilado">Jubilado</option>
       </select>
-
-      <p>{result}</p>
+      <br></br>
       <input type="submit" />
+
+      <table class="default">
+        <tr>
+          <th>Nombre</th>
+          <th>Edad</th>
+          <th>Ocupación</th>
+          <th>Categoría</th>
+        </tr>
+        {result.map((data) => {
+          return (
+            <tr>
+              <td>{data.Nombre}</td>
+              <td>{data.Edad}</td>
+              <td>{data.Ocupacion} </td>
+              <td>{data.Categoria} </td>
+            </tr>
+          );
+        })}
+      </table>
     </form>
   );
 }
